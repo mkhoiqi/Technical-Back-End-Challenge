0445961d8e2932bf66e0ed6a9efc2a459277df10
mkhoiqi <mkhoirurrizqi08@gmail.com>
2022-06-22 17:20:27 +0700

    result

--- sur_easy_higer_value.js
+++ sur_easy_higer_value.js
@@ -8,7 +8,19 @@
 let numbers = [3, 1, 2, 3, 7, 5, 6, 8, 2, 1];
 
 function result(numbers) {
-    // Your Code Here
+  // Your Code Here
+  let max;
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (i == 0) {
+      max = numbers[i];
+    } else {
+      if (numbers[i] > max) {
+        max = numbers[i];
+      }
+    }
+  }
+  return max;
 }
 
 console.log(result(numbers));
